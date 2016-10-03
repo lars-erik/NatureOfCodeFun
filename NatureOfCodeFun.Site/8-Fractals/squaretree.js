6caab6796278241b142494ab9993f42bfc630719
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2016-10-04 00:25:03 +0200

    parameters, sliders, colors, fun!

--- NatureOfCodeFun.Site/8-Fractals/squaretree.js
+++ NatureOfCodeFun.Site/8-Fractals/squaretree.js
@@ -9,18 +9,59 @@
         cutoff = .65,
         angle = Math.PI / 5,
         maxIterations = 6,
+        branches = 3,
+        startColor = [72, 33, 7],
+        endColor = [62, 97, 7],
+        colorDiffs = [
+                endColor[0] - startColor[0],
+                endColor[1] - startColor[1],
+                endColor[2] - startColor[2]
+        ],
+        color = startColor,
         length = startLength
     ;
+
+    function iterationColor(iteration) {
+        var step = maxIterations - iteration,
+            stepFrac = step / maxIterations;
+
+        color = [
+            Math.round(startColor[0] + colorDiffs[0] * stepFrac),
+            Math.round(startColor[1] + colorDiffs[1] * stepFrac),
+            Math.round(startColor[2] + colorDiffs[2] * stepFrac)
+        ];
+    }
+
+    function hexColor() {
+        var r = color[0].toString(16),
+            g = color[1].toString(16),
+            b = color[2].toString(16);
+        r = r.length === 1 ? "0" + r : r;
+        g = g.length === 1 ? "0" + g : g;
+        b = b.length === 1 ? "0" + b : b;
+        return "#" + r + g + b;
+    }
 
     function resized() {
         width = canvas.width = window.innerWidth - 200;
         height = canvas.height = window.innerHeight;
     }
 
+    function updateValue(id, value) {
+        eval(id + " = " + value);
+        main();
+    }
+
     function inputChanged() {
         var name = this.id,
             value = eval(this.value);
-        eval(name + " = " + value);
+        updateValue(name, value);
+    }
+
+    function sliderHandler(id) {
+        return function(vp) {
+            updateValue(id, vp.newValue);
+        }
     }
 
     function initialize() {
@@ -28,6 +69,12 @@
             i;
 
         for (i = 0; i < inputs.length; i++) {
+            $(inputs[i]).slider({
+                value: Number(inputs[i].value)
+            });
+
+            $(inputs[i]).slider("on", "change", sliderHandler(inputs[i].id));
+
             inputs[i].addEventListener("change", inputChanged);
         }
 
@@ -50,7 +97,7 @@
         gfx.lineTo(pos.re + length * startWidthOffset / 2, pos.im - length);
         gfx.lineTo(pos.re - length * startWidthOffset / 2, pos.im - length);
         gfx.closePath();
-        gfx.fillStyle = "white";
+        gfx.fillStyle = hexColor();
         gfx.fill();
 
         if (iterations > 0) {
@@ -67,20 +114,22 @@
     }
 
     function drawBranches(iterations) {
-        gfx.save();
-        gfx.rotate(angle);
-        drawBranch(iterations);
-        gfx.restore();
+        var i,
+            curAng = angle * -1,
+            incAng = angle * 2 / (branches - 1);
 
-        drawBranch(iterations);
-
-        gfx.save();
-        gfx.rotate(angle*-1);
-        drawBranch(iterations);
-        gfx.restore();
+        for (i = 0; i < branches; i++) {
+            gfx.save();
+            iterationColor(iterations);
+            gfx.rotate(curAng);
+            drawBranch(iterations);
+            gfx.restore();
+            curAng += incAng;
+        }
     }
 
     function main() {
+        color = startColor;
         gfx.clearRect(0, 0, width, height);
         gfx.save();
 
@@ -90,7 +139,7 @@
         drawBranch(maxIterations);
 
         gfx.restore();
-        window.requestAnimationFrame(main);
+        //window.requestAnimationFrame(main);
     }
 
     initialize();
