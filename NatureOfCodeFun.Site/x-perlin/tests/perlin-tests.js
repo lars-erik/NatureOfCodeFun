b090e2621544b90c3dc51b075ded69905d4c1e08
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2016-10-27 00:40:47 +0200

    ooohh... plasma :)

--- NatureOfCodeFun.Site/x-perlin/tests/perlin-tests.js
+++ NatureOfCodeFun.Site/x-perlin/tests/perlin-tests.js
@@ -32,4 +32,41 @@
         });
     });
 
+    describe("when intializing 3d", function () {
+
+        it("should create random gradient for each whole grid vertice", function () {
+            function gradInRange(gradient) {
+                inRange(gradient[0]);
+                inRange(gradient[1]);
+                inRange(gradient[2]);
+            }
+
+            var set = new noise.perlin(2, 3);
+
+            gradInRange(set.gradient(0, 0, 0));
+            gradInRange(set.gradient(1, 1, 1));
+        });
+
+    });
+
+    describe("when getting a 3d coordinate", function () {
+        it("should have a value", function () {
+            var set = new noise.perlin(2, 3),
+                value = set.noise([0.5, 0.5, 0.5]);
+            inRange(value);
+        });
+
+        it("adjacent on z should not be equal", function () {
+            var set = new noise.perlin(2, 3),
+                value1 = set.noise([0.5, 0.5, 0.5]),
+                value2 = set.noise([0.5, 0.5, 0.6])
+            ;
+            console.log(value1);
+            console.log(value2);
+            expect(value1).not.toEqual(value2);
+        });
+    });
+
+
+
 }());
