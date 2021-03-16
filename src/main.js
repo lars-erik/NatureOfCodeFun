bd167f78aae29bb3f1ae3b17eda33616951eac7a
Lars-Erik Aabech <lea@markedspartner.no>
2021-03-16 23:59:18 +0100

    Allow reset of app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import p5 from "p5";
 
+const container = document.getElementById("container");
+
+
 let sketch = function(p) {
+
+
     let width = window.innerWidth;
-    let height = window.innerHeight;
+    let height = window.innerHeight - 50;
 
     let x = 100;
     let y = 100;
@@ -10,27 +15,32 @@ let sketch = function(p) {
     let ySpeed = 10;
 
     p.setup = function() {
-            p.createCanvas(width, height);
-            p.background(255);
-        },
+        p.createCanvas(width, height);
+        p.background(255);
+    };
 
-        p.draw = function() {
-            p.background(255);
+    p.draw = function() {
+        p.background(255);
 
-            x += xSpeed;
-            y += ySpeed;
+        x += xSpeed;
+        y += ySpeed;
 
-            if ((x > width) || (x < 0)) {
-                xSpeed *= -1;
-            }
-            if ((y > height) || (y < 0)) {
-                ySpeed *= -1;
-            }
-
-            p.stroke(0);
-            p.fill(175);
-            p.ellipse(x, y, 16, 16);
+        if ((x > width) || (x < 0)) {
+            xSpeed *= -1;
         }
+        if ((y > height) || (y < 0)) {
+            ySpeed *= -1;
+        }
+
+        p.stroke(0);
+        p.fill(175);
+        p.ellipse(x, y, 16, 16);
+    }
 }
 
-new p5(sketch, 'container');
+let engine = new p5(sketch, container);
+
+document.getElementById("reset").addEventListener("click", () => {
+    engine.remove();
+    engine = new p5(sketch, container);
+});
