8cb3dcf9544a763af6bf90c3e3646a8259fe0dc8
Lars-Erik Aabech <lea@markedspartner.no>
2021-03-17 00:02:05 +0100

    1.2 Refactored 1.1 to vectors

--- src/main.js
+++ src/main.js
@@ -2,17 +2,12 @@ import p5 from "p5";
 
 const container = document.getElementById("container");
 
-
 let sketch = function(p) {
-
-
     let width = window.innerWidth;
     let height = window.innerHeight - 50;
 
-    let x = 100;
-    let y = 100;
-    let xSpeed = 3;
-    let ySpeed = 10;
+    let location = new p5.Vector(100, 100);
+    let velocity = new p5.Vector(3, 10);
 
     p.setup = function() {
         p.createCanvas(width, height);
@@ -22,19 +17,18 @@ let sketch = function(p) {
     p.draw = function() {
         p.background(255);
 
-        x += xSpeed;
-        y += ySpeed;
+        location.add(velocity);
 
-        if ((x > width) || (x < 0)) {
-            xSpeed *= -1;
+        if ((location.x > width) || (location.x < 0)) {
+            velocity.x *= -1;
         }
-        if ((y > height) || (y < 0)) {
-            ySpeed *= -1;
+        if ((location.y > height) || (location.y < 0)) {
+            velocity.y *= -1;
         }
 
         p.stroke(0);
         p.fill(175);
-        p.ellipse(x, y, 16, 16);
+        p.ellipse(location.x, location.y, 16, 16);
     }
 }
 
